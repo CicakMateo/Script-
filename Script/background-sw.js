f2a7b7442a064c41834bff87943cdec7387f1bea
CicakMateo <106764043+CicakMateo@users.noreply.github.com>
2025-09-16 17:24:54 +0200

    Update background-sw.js

--- Script/background-sw.js
+++ Script/background-sw.js
@@ -1,5 +1,44 @@
-// Polyfill laden
-importScripts("libraries/browser-polyfill.min.js");
+let converting = false;
+let converting_tab = -1;
+let convert_progress = {
+  title: "",
+  from_page: -1,
+  cur_page: -1,
+  to_page: -1,
+  page_count: -1,
+  time_begin: 0
+};
 
-// Hintergrundskript laden
-importScripts("scripts/background.js");
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message.type) {
+    case "start_converting":
+      converting = true;
+      convert_progress = message.convert_progress;
+      break;
+    case "update_tabid":
+      converting_tab = message.tabid;
+      break;
+    case "is_converting":
+      sendResponse({ converting, converting_tab });
+      break;
+    case "get_progress":
+      sendResponse({ convert_progress });
+      break;
+    case "stop_converting":
+      converting = false;
+      converting_tab = -1;
+      break;
+    case "update_progress":
+      convert_progress = message.convert_progress;
+      if (convert_progress.cur_page === convert_progress.to_page) {
+        converting = false;
+        converting_tab = -1;
+      }
+      break;
+    case "cancel_convert":
+      converting = false;
+      converting_tab = -1;
+      break;
+  }
+  return true; // async response ok
+});
